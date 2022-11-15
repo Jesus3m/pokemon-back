05227a4e343e0e57fd7bdc5e1adb21279617fbfa
Jesus Moreno <jesus.moreno@uruit.com>
2022-11-14 20:07:27 -0500

    Auth Middleware

--- src/common/middlewares/authMiddelware.ts
+++ src/common/middlewares/authMiddelware.ts
@@ -3,12 +3,13 @@ import { verifyToken } from '@core/auth/usecases/verifyToken'
 import { NextFunction, Request, Response } from 'express'
 
 export const withAuth = async (req: Request, res: Response, next: NextFunction) => {
-    // if (!req.headers.authorization) {
-    //     throw new HttpError('Authorization token is required')
-    // }
-    // const verifiedToken = await verifyToken(req.headers.authorization!) as { id: string, lastConnection: Date}
-    // if (!verifiedToken?.id) {
-    //     throw new HttpError('Token Invalid')
-    // }
+    if (!req.headers.authorization) {
+        throw new HttpError('Authorization token is required')
+    }
+    const token = req.headers.authorization.split('Bearer ')
+    const verifiedToken = await verifyToken(token[1]) as { id: string, lastConnection: Date}
+    if (!verifiedToken?.id) {
+        throw new HttpError('Token Invalid')
+    }
     next()
 }
